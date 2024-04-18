56a8ad71903c1bd93137128743f539a980079636
Abhishek Rawat <richard4u343@gmail.com>
2024-04-18 09:02:53 +0530

    added animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,48 @@
-import React, { useEffect } from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-
-import Contact from './components/Contact'
-import { useSelector } from 'react-redux'
-import AOS from 'aos';
-import 'aos/dist/aos.css'
-import Details from './components/Details'
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import { useSelector } from "react-redux";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Details from "./components/Details";
+import Loder from "./components/Loder";
 
 const App = () => {
-  const light = useSelector((store)=> store.toggle.value)
+  const light = useSelector((store) => store.toggle.value);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    AOS.init()
-    
-  }, [])
-  
+    // Initialize AOS
+    AOS.init();
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    
+    <div className={"overflow-hidden"}>
+      {isLoading ? (
+        <div className={"loading-animation"}>
+          <Loder />
+        </div>
+      ) : (
+        <>
+          <Navbar light={light} />
+          <Hero light={light} />
+          <Details light={light} />
+          <Projects light={light} />
+          <Skills light={light} />
+          <Contact light={light} />
+        </>
+      )}
+    </div>
+  );
+};
 
-      <div className=' overflow-hidden'>
-      <Navbar light={light}/>
-      <Hero light={light}/>
-      <Details  light={light}/>
-      <Projects light={light}/>
-      <Skills light={light}/>
-      <Contact light={light}/>
-      
-     
-     </div> 
-    
-    
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/Loder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import loder from '../assets/load.gif'
+
+const Loder = () => {
+  return (
+    <div className='h-screen w-full bg-black flex items-center justify-center'>
+        <img src={loder} alt="" />
+    </div>
+  )
+}
+
+export default Loder
